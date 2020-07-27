52f229df584e9e72d8be1592036e5fed4b35395d
Leena <leena.9314@gmail.com>
2020-07-27 12:12:20 +0530

    table displayed in tabs

--- src/app/components/js/tabs/tabs.jsx
+++ src/app/components/js/tabs/tabs.jsx
@@ -6,7 +6,9 @@ import AppBar from '@material-ui/core/AppBar';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
 import Typography from '@material-ui/core/Typography';
+import EnhancedTable from '../dashboard/table';
 import Box from '@material-ui/core/Box';
+import Dashboard from '../dashboard/dashboard';
 
 function TabPanel(props) {
   const { children, value, index, ...other } = props;
@@ -72,11 +74,11 @@ export default function TabsNav() {
           variant="fullWidth"
           aria-label="full width tabs example"
         >
-          <Tab label="Item One" {...a11yProps(0)} />
-          <Tab label="Item Two" {...a11yProps(1)} />
-          <Tab label="Item Three" {...a11yProps(2)} />
-          <Tab label="Item Four" {...a11yProps(3)} />
-          <Tab label="Item Five" {...a11yProps(4)} />
+          <Tab label="Unapproved User" {...a11yProps(0)} />
+          <Tab label="Temp Card user" {...a11yProps(1)} />
+          <Tab label="Today" {...a11yProps(2)} />
+          <Tab label="This Month" {...a11yProps(3)} />
+          <Tab label="Ongoing Visits" {...a11yProps(4)} />
         </Tabs>
       </AppBar>
       <SwipeableViews
@@ -85,19 +87,21 @@ export default function TabsNav() {
         onChangeIndex={handleChangeIndex}
       >
         <TabPanel value={value} index={0} dir={theme.direction}>
-          Item One
+        List of Un-Approved Visits for Facility
         </TabPanel>
         <TabPanel value={value} index={1} dir={theme.direction}>
-          Item Two
+        List of top 10 Employees Using Temporary Access for Facility
         </TabPanel>
         <TabPanel value={value} index={2} dir={theme.direction}>
-          Item Three
+        No of Visitors to Facility today
         </TabPanel>
         <TabPanel value={value} index={3} dir={theme.direction}>
-          Item Four
+        No of Visitors to Facility this month
+        <EnhancedTable />
         </TabPanel>
         <TabPanel value={value} index={4} dir={theme.direction}>
-          Item Five
+        List of Ongoing Visits for Facility
+        <Dashboard />
         </TabPanel>
       </SwipeableViews>
     </div>
